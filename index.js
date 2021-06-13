abf172ebb02fbb8cbd2ed4dbac007a0b9a07253a
charlie <itscharlieliu@gmail.com>
2021-06-13 11:59:49 -0700

    Add items to todo list

---
+++ index.js
@@ -0,0 +1,23 @@
+const todoLists = document.getElementsByClassName("todoList");
+
+for (const todoList of todoLists) {
+    const input = todoList.getElementsByClassName("todoList__input")[0];
+    const itemsList = todoList.getElementsByClassName("todoList__list")[0];
+
+    const addItemToList = (item) => {
+        const itemElement = document.createElement("SPAN");
+        itemElement.innerText = item;
+        itemElement.className = "todoList__listItem";
+
+        itemsList.appendChild(itemElement);
+    };
+
+    console.log(input);
+
+    input.addEventListener("change", function (event) {
+        addItemToList(event.target.value);
+        // Can use 'this' context because it the 'object' is the input element
+        // Will not work using arrow function
+        this.value = "";
+    });
+}
